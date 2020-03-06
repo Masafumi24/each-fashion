ae69a3413e9edbf007a1d49f648490c48d93a473
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-03-06 18:55:03 +0900

    kkkk

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  # validates :image, presence: true
+
+  # mount_uploader :image, ImageUploader
+  belongs_to :user
+end
---
+++ db/migrate/20200304015359_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :image
+      t.text :text
+      t.timestamps
+
+      def change
+        change_column :images, :binary_data, :binary, :limit => 5.megabyte
+      end
+    end
+  end
+end
