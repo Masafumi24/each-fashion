bbc5e759ca3a73f6a4a85fb7059e21320480501a
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-08 11:33:41 +0900

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
+  resources :users, only: [:show, :edit]
   resources :posts do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: :create
   end
-  resources :users, only: [:show, :edit]
 end
